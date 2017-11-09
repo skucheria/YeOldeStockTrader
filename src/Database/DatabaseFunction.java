d3ae1b1e16ad910ef77ce43e33714efe6d047abd
skucheria <siddharth@kucheria.com>
2017-11-08 17:12:45 -0800

    added jdbc package, class, and jar file

---
+++ src/Database/DatabaseFunction.java
@@ -0,0 +1,26 @@
+package Database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DatabaseFunction {
+	private static Connection conn = null;
+	private static ResultSet rs = null;
+	private static PreparedStatement ps = null;
+	
+	public static void connect(){
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection("jdbc:mysql://localhost/Lab10?user=root&password=root&useSSL=false");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+}
